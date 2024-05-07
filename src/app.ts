c6565b4425d3b8884cc654ebf67f27cf88579ea0
Bradley Potzka <bpotzka@amazon.com>
2024-05-06 22:05:28 -0700

    switch to fs.promises

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
-import * as fs from "fs";
+import * as fs from "fs/promises";
 import { KEY_TO_WEIGHT_MAP } from "./types";
 import { OVERWATCH_ROLES } from "./constants";
 
 const WEIGHTED_FILE_NAME = "src/assets/weights.json";
+const FILE_NOT_FOUND_ERR_CODE = "ENOENT";
 
 const intializeKeysWithWeights = (keys: string[]): KEY_TO_WEIGHT_MAP => {
   const result: KEY_TO_WEIGHT_MAP = {};
@@ -24,14 +25,15 @@ const updateWeights = (allKeys: string[], selectedKey: string, map: KEY_TO_WEIGH
   });
 };
 
-const readAndWriteData = (data?: Buffer) => {
+const readAndWriteData = async (data?: Buffer) => {
   const desiredKeys = Object.values(OVERWATCH_ROLES);
   let weightedMap: KEY_TO_WEIGHT_MAP = data ? JSON.parse(data.toString()) : intializeKeysWithWeights(desiredKeys);
   console.log("Current weights: ", weightedMap);
 
-  const allWeights = Object.values(weightedMap)
-    .map((weight, index) => Array(weight).fill(index))
-    .flat();
+  const allWeights = Object.values(weightedMap).reduce(
+    (accumulatedWeight, weight, index) => accumulatedWeight.concat(Array<number>(weight).fill(index)),
+    [] as number[],
+  );
 
   const rnd = Math.floor(Math.random() * allWeights.length);
 
@@ -41,19 +43,24 @@ const readAndWriteData = (data?: Buffer) => {
   updateWeights(desiredKeys, result, weightedMap);
   console.log("New weights: ", weightedMap);
 
-  fs.writeFileSync(WEIGHTED_FILE_NAME, JSON.stringify(weightedMap));
+  await fs.writeFile(WEIGHTED_FILE_NAME, JSON.stringify(weightedMap));
 
   console.log("Result: ", result);
 };
 
-const generateChoice = () => {
+const generateChoice = async () => {
   try {
-    const file = fs.readFileSync(WEIGHTED_FILE_NAME);
+    const file = await fs.readFile(WEIGHTED_FILE_NAME);
     console.log("Found existing data");
+
     readAndWriteData(file);
-  } catch (e) {
-    console.log("Did not find existing data. Creating new data...");
-    readAndWriteData();
+  } catch (err) {
+    if (err.code === FILE_NOT_FOUND_ERR_CODE) {
+      console.log("Did not find existing data. Creating new data...");
+      readAndWriteData();
+    } else {
+      console.log("Failed to open or create file: ", err);
+    }
   }
 };
 
